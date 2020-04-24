eed30bc1fa001c37125d482c390770bfa66ed9b3
Bharath banothu <bbanothu1997@gmail.com>
2020-04-23 21:37:55 -0700

    refactor

--- TicTacToe/src/Server/Game.java
+++ TicTacToe/src/Server/Game.java
@@ -1,76 +1,51 @@
-package Server; 
+package Server;
+
 import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.Scanner;
-import java.util.StringTokenizer;
-
 import application.TicTacToe;
 
 // ClientHandler class 
-class Game implements Runnable  
-{ 
-
-
-    
-    Socket p1;
+class Game implements Runnable {
+	TicTacToe game;
+	Socket p1;
+	Socket p2;
 	private PrintWriter p1Out;
-	private BufferedReader p1In;
-	
-	
-    
-    Socket p2;
 	private PrintWriter p2Out;
+	private BufferedReader p1In;
 	private BufferedReader p2In;
-	
-	
-    TicTacToe game;
-      
-    // constructor 
-
-  
 
 	public Game(Socket p1, Socket p2, PrintWriter p1Out, PrintWriter p2Out, BufferedReader p1In, BufferedReader p2In) {
-		this.p1 = p1; 
-        this.p1Out = p1Out; 
-        this.p1In = p1In; 
-        
-        
-        this.p2 = p2; 
-        this.p2Out = p2Out; 
-        this.p2In = p2In; 
-
+		this.p1 = p1;
+		this.p2 = p2;
+		this.p1Out = p1Out;
+		this.p1In = p1In;
+		this.p2Out = p2Out;
+		this.p2In = p2In;
 	}
 
-
-	
-	private void sendMessage(PrintWriter out , String msg) {
+	private void sendMessage(PrintWriter out, String msg) {
 		out.println(msg);
 		out.write(msg);
 		out.flush();
 	}
-	
-	
+
 	@Override
-    public void run() { ;
-    sendMessage(p1Out, "p1");
-    sendMessage(p2Out, "p2");
-    
-        while (true){ 
-        	try {
-        		String p1move = p1In.readLine();
-        		p1move = p1move.substring(p1move.length()-3); 
+	public void run() {
+		;
+		sendMessage(p1Out, "p1");
+		sendMessage(p2Out, "p2");
+		while (true) {
+			try {
+				String p1move = p1In.readLine();
+				p1move = p1move.substring(p1move.length() - 3);
 				sendMessage(p2Out, p1move);
-				
-        		String p2move = p2In.readLine();
-        		p2move = p2move.substring(p2move.length()-3); 
+				String p2move = p2In.readLine();
+				p2move = p2move.substring(p2move.length() - 3);
 				sendMessage(p1Out, p2move);
-				
 			} catch (IOException e) {
 			}
-        }
-    } 
-} 
+		}
+	}
+}
--- TicTacToe/src/Server/Server.java
+++ TicTacToe/src/Server/Server.java
@@ -4,25 +4,20 @@
 package Server; 
 import java.io.*; 
 import java.util.*;
-
 import application.TicTacToe;
-
 import java.net.*; 
   
 // Server class 
 public class Server  { 
-
     static Vector<User> ar = new Vector<>(); 
     static int i = 0; 
-  
     public static void main(String[] args) throws IOException  { 
         @SuppressWarnings("resource")
 		ServerSocket Listen = new ServerSocket(1234); 
         Socket newConnection; 
-        while (true)  
-        { 
+        while (true){ 
         	newConnection = Listen.accept(); 
-            System.out.println("New client request received : " + newConnection); 
+            System.out.println("New client request received: " + newConnection); 
              
             // obtain input and output streams 
             BufferedReader inputStream = new BufferedReader(new InputStreamReader(newConnection.getInputStream()));
@@ -31,21 +26,19 @@ public class Server  {
             System.out.println("Creating a new handler for this client..."); 
             User x = new User(newConnection, inputStream, outputStream, ""); 
   
-              
+        
             System.out.println("Adding this client to active client list"); 
             ar.add(x); 
             i++;
   
             if(ar.size() >= 2) {
-            	TicTacToe board = new TicTacToe();
+            	//TicTacToe board = new TicTacToe();
             	User p1 = ar.get(0);
             	User p2 = ar.get(1);
             	p1.move = "X";
             	p2.move = "O";
             	ar.remove(p1);
             	ar.remove(p2);
-            	
-            	
             	Game game = new Game(p1.conn, p2.conn, p1.out, p2.out, p1.in, p2.in); 
                 Thread gameThread = new Thread(game);
                 gameThread.start(); 
